571055ea432fee63984f6f051a647c9f6abce0da
KJH <wo7474@naver.com>
2022-03-04 15:42:45 +0900

    Add files via upload

---
+++ WebTranslation/app/src/main/java/com/example/translation/ui/login/ui/login/LoginFragment.kt
@@ -0,0 +1,45 @@
+package com.example.translation.ui.login.ui.login
+
+import android.content.Intent
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import com.example.translation.R
+import com.example.translation.databinding.FragmentLoginBinding
+import com.example.translation.ui.register.RegisterActivity
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        register.setOnClickListener {
+            val intent : Intent = Intent(activity,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
